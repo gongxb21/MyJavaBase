d0ed76b1334f8b6727c06e9e2b4293bc4c4f30cc
gongxb21 <gongxb2260@outlook.com>
2018-01-21 20:19:35 +0800

    string format

--- src/base/StringTest.java
+++ src/base/StringTest.java
@@ -7,8 +7,21 @@ import java.util.Observer;
  */
 public class StringTest {
 	public static void main(String[] args) {
+		//intern();
+		format();
+	}
+
+	/**
+	 * 
+	 * void
+	 */
+	private static void intern() {
 		String s="helloworld";
 		// intern 方法
 		System.out.println(s.intern());
+	}
+	
+	public static void format() {
+		System.out.println(String.format("价格%d元", 10));
 	}
 }
